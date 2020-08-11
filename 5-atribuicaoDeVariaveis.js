75c2c70a0df96353872e43bc7b8fe0b7f1e16325
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2020-08-11 16:21:41 -0300

    Add files via upload

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 37;
+const primeiroNome = "Tiago";
+const sobrenome = "Marinho"
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + " " + sobrenome;
+console.log(nomeCompleto);
+
+let contador = 0;
+contador = contador + 1;
+console.log(contador);
