0e59571344951893ef06274062cd581f15ff8b54
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2020-08-19 13:03:07 -0300

    Add files via upload

--- JS_Intr_POO/ContaCorrente.js
+++ JS_Intr_POO/ContaCorrente.js
@@ -1,14 +1,47 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0;//total de contas existentes
     agencia;
-    //saldo = 0;
+    _cliente;
+      //saldo = 0;
     _saldo = 0;
+    numDaConta;//numero atribuido a conta do cliente
+
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+  
+    get saldo(){
+        return this._saldo;
+    }
+
+
+    
+
+    constructor (agencia, cliente){
+        this.agencia = agencia;
+        this._cliente = cliente;
+        ContaCorrente.numeroDeContas +=1;//contador das contas
+        this.numDaConta = ContaCorrente.numeroDeContas + 505;//numero da conta, pode ser qualquer numero aleatório mais o contador
+    }
+
+
 
     sacar(valor) {
         if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         } else {
-            console.log("Saldo insuficiente! Seu saldo é R$", contaCorrenteTiago._saldo);
+            console.log("\nVocê tentou sacar R$", valor);
+            console.log("Saldo insuficiente! Seu saldo é R$", this._saldo);
         }
     }
 
@@ -17,5 +50,10 @@ export class ContaCorrente {
             return;
         }
         this._saldo += valor;
+    }
+
+    tranferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- JS_Intr_POO/index.js
+++ JS_Intr_POO/index.js
@@ -1,23 +1,21 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js"
-const cliente1 = new Cliente();
-cliente1.nome = "Tiago";
-cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente1 = new Cliente("Tiago", 11122233309);
+const conta1 = new ContaCorrente(1001, cliente1);
 
-const contaCorrenteTiago = new ContaCorrente();
-contaCorrenteTiago.agencia = 1001;
+const cliente2 = new Cliente("Alice", 88822233309);
+const conta2 = new ContaCorrente(1002, cliente2);
 
-contaCorrenteTiago.depositar(100);
-contaCorrenteTiago.depositar(100);
-contaCorrenteTiago.depositar(100);
 
-const valorSacado = contaCorrenteTiago.sacar(50);
-console.log("Você sacou R$", valorSacado);
+// //depositar e sacar
+// conta1.depositar(100);
+// //console.log("Seu saldo é:", conta1._saldo);
+// conta1.depositar(100);
+// conta1.depositar(100);
+// const valorSacado = conta1.sacar(50);
+conta1.depositar(500);
+conta1.tranferir(200, conta2);
 
-console.log(contaCorrenteTiago);
-
+console.log(conta2);
 
